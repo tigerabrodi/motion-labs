37741eab0a24bb48dced684bbc19989c639192b5
Tiger Abrodi <tigerabrodi@gmail.com>
2025-02-03 12:59:26 +0100

    fix: header responsiveness

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,15 +2,15 @@ import { Konoha } from '../icons/konoha'
 
 export function Header() {
   return (
-    <div className="flex w-full items-center justify-between py-16">
+    <div className="flex w-full flex-col items-center gap-2 py-4 lg:flex-row lg:justify-between lg:py-16">
       <Konoha className="fill-accent-500 size-14" />
 
-      <div className="flex flex-col gap-3">
-        <h1 className="font-ninja text-accent-500 flex items-center gap-4 text-5xl">
+      <div className="flex flex-col items-center gap-1 lg:items-start lg:gap-3">
+        <h1 className="font-ninja text-accent-500 flex items-center gap-4 text-3xl lg:text-5xl">
           <span>Motion</span>
           <span>Labs</span>
         </h1>
-        <p className="text-accent-500 font-ninja flex items-center gap-6 text-sm">
+        <p className="text-accent-500 font-ninja flex items-center gap-6 text-xs lg:text-sm">
           Built and desinged by{' '}
           <a
             href="https://github.com/tigerabrodi"
