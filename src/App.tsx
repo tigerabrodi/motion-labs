d75eb5bb782202a7cdd173845fcebaeccd30e308
Tiger Abrodi <tigerabrodi@gmail.com>
2025-02-05 10:27:49 +0100

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,15 @@ import { SiteHeader } from './components/site-header'
 const shouldShow = import.meta.env.VITE_SHOW_COMPONENTS as string | undefined
 
 function App() {
-  return (
-    <>
-      {shouldShow ? (
-        <main className="container mx-auto flex min-h-screen flex-col items-center">
-          <SiteHeader />
-          <ExperimentsList />
-        </main>
-      ) : (
-        <main className="flex min-h-screen items-center justify-center">
-          <p className="font-ninja text-accent-400 text-3xl">Coming soon</p>
-        </main>
-      )}
-    </>
+  return shouldShow ? (
+    <main className="container mx-auto flex min-h-screen flex-col items-center">
+      <SiteHeader />
+      <ExperimentsList />
+    </main>
+  ) : (
+    <main className="flex min-h-screen items-center justify-center">
+      <p className="font-ninja text-accent-400 text-3xl">Coming soon</p>
+    </main>
   )
 }
 
