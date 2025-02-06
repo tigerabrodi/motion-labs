fa84fccd727f8b5f3fee4433e2ed0ad0a28010e4
Tiger Abrodi <tigerabrodi@gmail.com>
2025-02-06 16:56:27 +0100

    fix ordering

--- src/components/experiments-list.tsx
+++ src/components/experiments-list.tsx
@@ -11,14 +11,14 @@ import { VoiceMessagePlayExperiment } from '../experiments/voice-message-play'
 export function ExperimentsList() {
   return (
     <div className="flex w-full flex-grow flex-col items-center gap-10 p-3 lg:gap-20 lg:py-8">
+      <CounterExperiment />
+      <CardsExperiment />
       <TodolistExperiment />
       <VoiceMessagePlayExperiment />
-      <CounterExperiment />
       <CreditCardExperiment />
       <TabsExperiment />
-      <InputButtonExperiment />
       <MultiStepFormExperiment />
-      <CardsExperiment />
+      <InputButtonExperiment />
       <SvgAnimationsExperiment />
     </div>
   )
